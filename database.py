49fddbc346d7164171029fcc9235733ca963740f
Solomon Sanderson <solomonsanderson@outlook.com>
2022-01-05 20:29:54 +0000

    added database, CG.db

--- database.py
+++ database.py
@@ -1,3 +1,9 @@
 '''
 This file uses SQLite to generate a database containing all of the Clepsh-Gordan Coefficients.
 '''
+
+
+import sqlite3
+
+
+con = sqlite3.connect('CG.db')
--- interface.py
+++ interface.py
@@ -1,3 +1,6 @@
 '''
 This file uses PyQt5 to create an interface to get and display the coefficients, using text entry boxes and buttons.
 '''
+
+
+import PyQt5
