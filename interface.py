5f11aaa26cd7cc2393b9a1f578f711cf3aa72596
Solomon Sanderson <solomonsanderson@outlook.com>
2022-01-05 21:00:14 +0000

    started developing interface.py
    basic window with icon now shows

--- interface.py
+++ interface.py
@@ -3,4 +3,23 @@ This file uses PyQt5 to create an interface to get and display the coefficients,
 '''
 
 
+import sys
 import PyQt5
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import *
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent = None):
+        super().__init__(parent)
+
+        self.setWindowTitle("Clepsh-Gordan Coefficient finder")
+        self.setFixedSize(400, 300)
+        self.setWindowIcon(QtGui.QIcon("icon.png"))
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
+
+
